21aa7709d562f8f333ba4eeb810d4d694e8749b7
Anton Lövgren <anton@lovgren.dev>
2025-01-03 21:35:16 +0100

    feat: add bootstrap

--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("actionButton");
-    button.addEventListener("click", () => {
-      alert("Hej! Knappen fungerar.");
-    });
-  });
-  
